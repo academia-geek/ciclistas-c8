cecc683b5e98749776d86159b6ca0563fc003283
devagiloscarmesa <oscar.mesa@agileinnova.org>
2021-07-12 13:00:18 -0500

    condicionales

--- js/index.js
+++ js/index.js
@@ -1,33 +1,11 @@
-/* console.log("Consulta")
-console.log("1. diferencia entre objetos y arreglos en JS")
-console.log("2. 5 metodos que se puedan usar con arreglos. ") */
+const ciclistas = [5, 7, 1, "oscar"];
 
-var mivariable = 3
-let nombre = "oscar"
-let apellido = 'MESA'
-const PI = 3.1416
-const esDeportista = true
-var a = 4
-let b = 5
-const suma = 4 + 5
-console.log(mivariable, nombre.toUpperCase(), apellido.toLowerCase(), PI, esDeportista);
-console.log(nombre.charAt(1), apellido.substr(-2, 1))
-console.log(a, '+', b, "=", suma)
+let x = 5 ,  y = 5
     
-
-
-const misuma = a => {
-    console.log("Estoy sumando!!!!")
-    const y = 10
-    if(mivariable > 1){
-        var x = 8
-        x = 2
-    }
-    console.log(a , "+", b, "=",  (a + b))
-    return 5 + a
+if( y > x ){
+    console.log("y es mayor que x")
+}else if(x === y){
+    console.log("y es igual x")
+}else{
+    console.log("x es mayor y")
 }
-
-const sumar = (a, b) => 
-     a + b
-
-console.log("mi suma es ", sumar(3, 4))
