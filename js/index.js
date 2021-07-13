b3663f8556c571c7beb3b4c64b05362722a873a8
devagiloscarmesa <oscar.mesa@agileinnova.org>
2021-07-13 08:43:06 -0500

    manejo de condicionales

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const ciclistas = [5, 7, 1, "oscar"];
 
-let x = 5 ,  y = 5
-    
-if( y > x ){
+let x = 5,  y = 4
+let cont = 1
+if( y > x || (++cont) === 2 ){
     console.log("y es mayor que x")
 }else if(x === y){
     console.log("y es igual x")
 }else{
     console.log("x es mayor y")
 }
+
+console.log(cont)
